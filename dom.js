118291f37fc37dd81b148d092ae77f4a0463f889
dgkri <dgkri0810@gmail.com>
2019-04-02 18:14:51 +0530

    final addition

--- dom.js
+++ dom.js
@@ -1,11 +1,10 @@
-
-
-
+// function for logging the ids
 function show(){
-  let _object = document.getElementsByTagName('div');
-  let l = _object.length;
+  let _object = document.getElementsByTagName('div'); //getting the object
+  let l = _object.length; //calculating the length of the object
 
-  for(let i=1 ; i<=l ; i+=1){
+  for(let i=1 ; i<=l ; i+=1){ //using the for loop for for printing the IDs
+    console.log("Here are the rectangle IDs")
     setTimeout(() =>{
       console.log(_object[i-1].id)
     }, i*1000)
