8164d6ebcc16346a98a51f5151fbffe88f1bdc69
dgkri <dgkri0810@gmail.com>
2019-06-18 16:42:38 +0530

    last commit

--- reverse.js
+++ reverse.js
@@ -2,6 +2,7 @@
 
 function reversejsonArray(str){
     try{
+        
         let arr = JSON.parse(str);
         let new_arr = [];
 
@@ -12,6 +13,7 @@ function reversejsonArray(str){
         new_arr = JSON.stringify(new_arr);
         
         return new_arr;
+         
     }
     catch(e){
         // console.log(e);
@@ -22,8 +24,19 @@ function reversejsonArray(str){
 let _a = [1, 2, 3, 4];
 a = JSON.stringify(_a);
 
+// with proper stringified array
 let rev_a = reversejsonArray(a);
 console.log(rev_a);
 
+// with an array not stringified
 let b = reversejsonArray(_a);
 console.log(b);
+
+// with no argument
+console.log(reversejsonArray());
+
+// with bool
+console.log(reversejsonArray(true));
+
+//with string
+console.log(reversejsonArray("hello"));
